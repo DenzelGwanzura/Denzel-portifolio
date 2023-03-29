39d60465c1fef07850db5f33b0de6491f356acd7
DenzelGwanzura <117734390+DenzelGwanzura@users.noreply.github.com>
2023-03-29 10:10:56 +0200

    Add files via upload

---
+++ DENZEL PORTIFOLIO2/scripts/script.js
@@ -0,0 +1,18 @@
+$('#toggleDark').click(function(){
+
+    if($("body").hasClass("dark-theme")){
+
+        $("body").removeClass("dark-theme");
+
+    }else{
+        $("body").addClass("dark-theme");
+    }
+});
+
+
+
+
+
+
+
+    
